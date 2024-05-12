5467a5bf491b33d277f1c7de894dab8a13840ae5
shenori <it22150448@my.sliit.lk>
2024-05-12 20:47:07 +0530

    create TaskAdapter.kt file to get data in database

---
+++ app/src/main/java/com/example/mobiletaskmgr/TaskAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.mobiletaskmgr
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskAdapter (private var tasks:List<Task>, context: Context):
+    RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+
+    class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
+        val descriptionTextView: TextView = itemView.findViewById(R.id.descTextView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.task_item, parent, false)
+        return TaskViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = tasks.size
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val task = tasks[position]
+        holder.nameTextView.text = task.name
+        holder.descriptionTextView.text = task.description
+    }
+
+    fun refreshData(newTasks: List<Task>){
+        tasks = newTasks
+        notifyDataSetChanged()
+    }
+}
