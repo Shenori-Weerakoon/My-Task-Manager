8f7d6faabc852bf8910800dec8558f5b5bb7597e
shenori <it22150448@my.sliit.lk>
2024-05-12 19:34:40 +0530

    create database and implement addtaskactivity.kt file

--- app/src/main/java/com/example/mobiletaskmgr/AddTaskActivity.kt
+++ app/src/main/java/com/example/mobiletaskmgr/AddTaskActivity.kt
@@ -1,20 +1,33 @@
 package com.example.mobiletaskmgr
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mobiletaskmgr.databinding.ActivityAddTaskBinding
+import com.example.mobiletaskmgr.databinding.ActivityMainBinding
 
 class AddTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddTaskBinding
+    private lateinit var db: TaskDatabaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_add_task)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = TaskDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener{
+            val name = binding.editTaskName.text.toString()
+            val description = binding.editDesc.text.toString()
+            val task = Task(0, name, description)
+            db.insertTask(task)
+            finish()
+            Toast.makeText(this, "Task Saved", Toast.LENGTH_SHORT).show()
+
         }
     }
 }
