55af96ab0f6d10bd4f0c3e7b945bd0c7a4442923
shenori <it22150448@my.sliit.lk>
2024-05-12 21:33:55 +0530

    get data from database

--- app/src/main/java/com/example/mobiletaskmgr/MainActivity.kt
+++ app/src/main/java/com/example/mobiletaskmgr/MainActivity.kt
@@ -6,16 +6,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mobiletaskmgr.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db: TaskDatabaseHelper
+    private lateinit var tasksAdapter: TaskAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        db = TaskDatabaseHelper(this)
+        tasksAdapter = TaskAdapter(db.getAllTasks(), this)
+
+        binding.tasksRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.tasksRecyclerView.adapter = tasksAdapter
 
 
         binding.addButton.setOnClickListener{
@@ -23,5 +32,10 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        tasksAdapter.refreshData(db.getAllTasks())
     }
 }
